3e0660d9c6132da4319174019a498a269c2e1299
Chamaramadushankadev <c.madushankaofficial@gmail.com>
2025-07-12 14:16:51 +0530

    CICD

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use(cors({ origin: '*' }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-// MongoDB connection
+// MongoDB connection 
 let isMongoConnected = false;
 
 const connectToMongoDB = async () => {

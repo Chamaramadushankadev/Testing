6c2495c2d10de87a5d55338d81c365fa983d2182
Chamaramadushankadev <c.madushankaofficial@gmail.com>
2025-07-17 15:27:59 +0530

    deployment testing 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
-    hmr: false, // Disable hot module replacement
+    host: '0.0.0.0', // allow external access
+    port: 5173,
+    hmr: {
+      protocol: 'ws',
+      host: 'studionexa.online',
+      port: 5173,
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:5002',

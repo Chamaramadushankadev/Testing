095f5a636efa58ad107d5ec18d89fe2d1091c148
Chamaramadushankadev <c.madushankaofficial@gmail.com>
2025-07-19 01:19:03 +0530

    mobile search button fixed

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -54,7 +54,7 @@ export const Header: React.FC<HeaderProps> = ({ activeTab }) => {
         </div>
         
         <div className="flex items-center space-x-4">
-          <div className="relative">
+          <div className="relative mr-4">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 dark:text-gray-500 w-4 h-4" />
             <input
               type="text"
